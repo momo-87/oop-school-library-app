3e2dc17fc3e30f9f089f6309f46b246fb0ea3faf
Momo-87 <momochristian87@gmail.com>
2023-07-24 11:35:17 +0100

    Create class Student

---
+++ student.rb
@@ -0,0 +1,14 @@
+require "./person.rb"
+
+class Student < Person
+
+  def initialize (classroom)
+    super(age, name = "Unknown", parent_permission = true)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
+end
