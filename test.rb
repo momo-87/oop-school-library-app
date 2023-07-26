5a6847391432f3253f63848085cf7e47bd776144
Momo-87 <momochristian87@gmail.com>
2023-07-26 14:59:36 +0100

    Create a class Book

--- test.rb
+++ test.rb
@@ -1,14 +1,22 @@
 require "./classroom.rb"
 require "./student.rb"
+require "./book.rb"
 
-momo = Student.new(36, "L1", "Momo")
-room = Classroom.new("L1")
-room.add_student(momo)
-# p momo.classroom
-p room.students
+# momo = Student.new(36, "L1", "Momo")
+# room = Classroom.new("L1")
+# room.add_student(momo)
+# # p momo.classroom
+# p room.students
 
 
-christian = Student.new(20, "M1", "Christian")
-christian.classroom = room
-# p christian.classroom
-p room.students
+# christian = Student.new(20, "M1", "Christian")
+# christian.classroom = room
+# # p christian.classroom
+# p room.students
+
+book = Book.new("Hello", "Momo")
+
+p book
+p book.title
+book.title = "Ho"
+p book.title
