ae2cec2c50288ddf6ccaa3d03527491a0a163783
momo-87 <momochristian87@gmail.com>
2023-07-31 11:22:24 +0100

    Refactore the main.rb file to meet the SOLID requirements

---
+++ start.rb
@@ -0,0 +1,44 @@
+require './app.rb'
+
+class Start
+  
+  def call_options
+    {
+      '1' => -> { @app.list_all_books },
+      '2' => -> { @app.list_all_people },
+      '3' => lambda {
+              @app.create_a_person
+              puts "Person created successfully\n"
+            },
+      '4' => lambda {
+              @app.create_a_book
+              puts "Book created successfully\n"
+            },
+      '5' => lambda {
+              @app.create_a_rental
+              puts "Rental created successfully\n"
+            },
+      '6' => -> { @app.list_all_rentals_for_a_person },
+      '7' => -> { puts 'Thank you for using this app!' }
+    }
+  end
+
+  def execute
+    @app = App.new
+    main = Main.new
+    loop do
+      main.display_menu
+      calls = call_options
+      choice = gets.chomp
+      if calls[choice]
+        calls[choice].call
+        return if choice == '7'
+      else
+        puts 'Sorry, the provided option does not exist'
+      end
+  
+      puts ' '
+    end
+  end
+end
+
