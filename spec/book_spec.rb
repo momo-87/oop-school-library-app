d955ccb33fa39489071a9f7b254e7b8e26699e2d
TamaraDimov <tamemojsoska@gmail.com>
2023-08-02 18:45:05 +0200

    test for book.rb

---
+++ spec/book_spec.rb
@@ -0,0 +1,26 @@
+require 'json'
+require_relative '../book'
+
+RSpec.describe Book do
+  let(:book_title) { 'The Great Gatsby' }
+  let(:book_author) { 'F. Scott Fitzgerald' }
+  let(:book) { Book.new(book_title, book_author) }
+
+  describe '#initialize' do
+    it 'creates a book with the correct title and author' do
+      expect(book.title).to eq(book_title)
+      expect(book.author).to eq(book_author)
+      expect(book.rentals).to be_an(Array)
+    end
+  end
+
+  describe '#add_rental' do
+    it 'adds a rental to the book and sets the book for the rental' do
+      rental = instance_double('Rental')
+      expect(rental).to receive(:book=).with(book)
+      book.add_rental(rental)
+      expect(book.rentals).to include(rental)
+    end
+  end
+
+end
